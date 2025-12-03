fe63becfe97883ef64eebd280a5eb24c39adfe17
kbudkiewicz <krystianbudkiewicz@gmail.com>
2025-12-03 14:08:37 +0100

    delete and deprecate unnecessary files

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,7 +1,9 @@
 import random
-import numpy as np
-import torch
 import yaml
+import torch
+import numpy as np
+from torch import Tensor
+
 
 def set_seed(seed: int = 42):
     random.seed(seed)
@@ -9,10 +11,20 @@ def set_seed(seed: int = 42):
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
 
+
 def save_yaml(obj, path):
     with open(path, "w") as f:
         yaml.safe_dump(obj, f)
 
+
 def load_yaml(path):
     with open(path, "r") as f:
         return yaml.safe_load(f)
+
+
+def get_relative_error(preds: Tensor, targets: Tensor) -> float:
+    return torch.mean(torch.abs(preds - targets) / (targets + 1e-8)).item()
+
+
+def get_abs_error(preds: Tensor, targets: Tensor) -> float:
+    return torch.mean(torch.abs(preds - targets)).item()
