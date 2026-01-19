e2fe05512a626377640d07c5bd1fbab1e3368227
kbudkiewicz <krystianbudkiewicz@gmail.com>
2026-01-19 14:28:48 +0100

    refactor: improve readability of the BaseDataset

--- data/datasets.py
+++ data/datasets.py
@@ -4,19 +4,22 @@ import nibabel as nib
 import torch
 import torch.nn.functional as F
 
-from typing import Tuple, Union
+from typing import Tuple, Union, Optional
+# from TPTBox import NII
 from torch import Tensor
 from torch.utils.data import Dataset
 
 
 class BaseDataset(Dataset):
-    def __init__(self, df, target_cols: str, augment: bool = False):
+    def __init__(self, df, target_cols: str, resolution: int = 128, augment: bool = False):
         if isinstance(df, str):
             self.df = pd.read_csv(df)
         else:
             self.df = df.reset_index(drop=True)
         self.target_cols =  self.get_targets(target_cols)
         self.cond_cols = self.get_targets('Ang|weight')
+        self.paths_key = self.get_targets('_path').values[0]
+        self.resolution = resolution
         self.augment = augment
 
     @property
@@ -27,23 +30,17 @@ class BaseDataset(Dataset):
     def name(self) -> str:
         return self.__class__.__name__.lower().replace('dataset', '')
 
-    @staticmethod
-    def _normalize(x: np.array) -> Tensor:
-        x = (x - np.mean(x)) / (np.std(x) + 1e-8)
-        x = torch.tensor(x).unsqueeze(0)
-        x = F.interpolate(x.unsqueeze(0), size=(128, 128, 128), mode="trilinear", align_corners=False).squeeze(0)
-        return x
+    @property
+    def std(self) -> Tensor:
+        vals = self.df[self.target_cols].values
+        return torch.std(torch.from_numpy(vals), dim=0)
 
     @staticmethod
-    def _import_mask(row, path: str) -> Tensor:
-        segmentation_mask = nib.load(
-            row["nifti_path"].replace("_ct.nii.gz", path), mmap=False
-        ).get_fdata().astype(np.float32)
-        segmentation_mask = torch.tensor(segmentation_mask).unsqueeze(0)
-        segmentation_mask = F.interpolate(
-            segmentation_mask.unsqueeze(0), size=(128, 128, 128), mode="trilinear", align_corners=False
-        ).squeeze(0)
-        return segmentation_mask
+    def _load_nifti(path: str) -> Tensor:
+        img = nib.load(path, mmap=False).get_fdata(dtype=np.float32)
+        img = np.nan_to_num(img, nan=0.0, posinf=0.0, neginf=0.0)
+        img = torch.tensor(img).unsqueeze(0)
+        return img
 
     @staticmethod
     def _augment(v: Tensor) -> Tensor:
@@ -54,6 +51,18 @@ class BaseDataset(Dataset):
         if torch.rand(1).item() < 0.2:
             v = v + 0.01 * torch.randn_like(v)
         return v
+
+    @staticmethod
+    def _normalize(x: np.array) -> Tensor:
+        x = (x - x.mean()) / (x.std() + 1e-8)
+        return x
+
+    def _interpolate(self, x: Tensor, size: Optional[int] = None) -> Tensor:
+        if isinstance(size, int):
+            size = (size, size, size)
+        else:
+            size = (self.resolution, self.resolution, self.resolution)
+        return F.interpolate(x.unsqueeze(0), size=size, mode='trilinear', align_corners=False).squeeze(0)
 
     def _mask_df(self, key: str, condition, negative: bool = False, inplace: bool = False) -> Union[None, pd.DataFrame]:
         mask = (self.df[key].values == condition)
@@ -69,29 +78,51 @@ class BaseDataset(Dataset):
         header = self.df.columns
         return header[header.str.contains(regex)]
 
+    def import_nii(self, path: str) -> Tensor:
+        img = self._load_nifti(path)
+        img = self._interpolate(img)
+        img = self._normalize(img)
+        if self.augment:
+            img = self._augment(img)
+        return img
+
+    def import_mask(self, path: str, suffix: str) -> Tensor:
+        path = path.replace('_ct.nii.gz', suffix)
+        mask = self._load_nifti(path)
+        mask = self._interpolate(mask)
+        return mask
+
     def import_img(self, idx: int) -> Tuple[Tensor, pd.DataFrame]:
         row = self.df.iloc[idx]
-        try:
-            img = nib.load(row["nifti_path"], mmap=False).get_fdata().astype(np.float32)
-            # print(f"Loaded image shape: {img.shape} from {row['nifti_path']}")
-            img = self._normalize(img)
-            if self.augment:
-                img = self._augment(img)
-            tissueSeg = self._import_mask(row, "_seg-tissue_msk.nii.gz")
-            spineSeg = self._import_mask(row, "_seg-spine_msk.nii.gz")
-            img = torch.cat([img, tissueSeg, spineSeg], dim=0)  # [3, D, H, W]
-            return img, row
-        except EOFError:
-            print(f"[WARNING] Skipping corrupted file: {row['nifti_path']}")
-            return self.__getitem__((idx + 1) % len(self.df))  # pick next one
+        img = self._load_nifti(row['nifti_path'])
+        img = self._interpolate(img)
+        img = self._normalize(img)
+        if self.augment:
+            img = self._augment(img)
 
-    def __getitem__(self, idx: int) -> Tuple[Tensor, ...]:
-        img, row = self.import_img(idx)
-        target = torch.tensor(row[self.target_cols].values.astype(np.float32))
+        tissueSeg = self.import_mask(row, "_seg-tissue_msk.nii.gz")
+        spineSeg = self.import_mask(row, "_seg-spine_msk.nii.gz")
+        img = torch.cat([img, tissueSeg, spineSeg], dim=0)  # [3, D, H, W]
+        return img, row
+        # except EOFError:
+        #     print(f"[WARNING] Skipping corrupted file: {row[self.paths_key]}")
+        #     return self.__getitem__((idx + 1) % len(self.df))  # pick next one
+
+    def __getitem__(self, idx: int) -> Tuple[Tensor, Tensor, float]:
+        selected_row = self.df.iloc[idx]
+        nifti_path = selected_row[self.paths_key]
+
+        img = self.import_nii(nifti_path)
+        tissue_mask = self.import_mask(nifti_path, '_seg-tissue_msk.nii.gz')
+        spine_mask = self.import_mask(nifti_path, '_seg-spine_msk.nii.gz')
+        img = torch.cat([img, tissue_mask, spine_mask], dim=0)
+        target = torch.tensor(selected_row[self.target_cols].values.astype(np.float32))
+
         try:
-            conditioning = torch.tensor(row[self.cond_cols].values.astype(np.float32))
+            conditioning = torch.tensor(selected_row[self.cond_cols].values.astype(np.float32))
         except KeyError:
             conditioning = torch.nan
+
         return img, target, conditioning
 
     def __len__(self):
