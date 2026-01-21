8c73fa53e182e6904a3c49a6e44f4ad7dd349927
kbudkiewicz <krystianbudkiewicz@gmail.com>
2026-01-21 15:00:02 +0100

    feat: add NAKO dataset classes

--- data/datasets.py
+++ data/datasets.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import pandas as pd
 import nibabel as nib
@@ -5,7 +6,7 @@ import torch
 import torch.nn.functional as F
 
 from typing import Tuple, Union, Optional
-# from TPTBox import NII
+from TPTBox import NII
 from torch import Tensor
 from torch.utils.data import Dataset
 
@@ -122,17 +123,177 @@ class BaseDataset(Dataset):
             conditioning = torch.tensor(selected_row[self.cond_cols].values.astype(np.float32))
         except KeyError:
             conditioning = torch.nan
-
         return img, target, conditioning
 
     def __len__(self):
         return len(self.df)
 
 
-class CTDataset(BaseDataset):
-    """For training a ResNet: img -> shear forces"""
-    def __init__(self, df):
-        super().__init__(df, target_cols="y")   # -> y0, ..., y4
+class NakoDataset(BaseDataset):
+    def __init__(self, df, target_cols: str = 'coord', augment: bool = False):
+        super().__init__(df, target_cols, augment)
+
+    @staticmethod
+    def _import_mask(row: str, pattern: str) -> Tensor:
+        mask_path = row.replace('nako_data', 'nako_msk')
+        mask_path = mask_path.replace('_T2w.nii.gz', pattern)
+        segmentation_mask = nib.load(mask_path, mmap=False).get_fdata().astype(np.float32)
+        segmentation_mask = torch.tensor(segmentation_mask).unsqueeze(0)
+        segmentation_mask = F.interpolate(
+            segmentation_mask.unsqueeze(0), size=(128, 128, 128), mode='trilinear', align_corners=False
+        ).squeeze(0)
+        return segmentation_mask
+
+    def import_img(self, idx: int) -> Tuple[Tensor, pd.DataFrame]:
+        row = self.df.iloc[idx]
+        data_path = row[self.paths_key]
+        img = nib.load(data_path, mmap=False).get_fdata().astype(np.float32)
+        img = self._normalize(img)
+        if self.augment:
+            img = self._augment(img)
+        spine_mask = self._import_mask(data_path, '_mod-T2w_seg-spine_msk.nii.gz')
+        vert_mask = self._import_mask(data_path, '_mod-T2w_seg-vert_msk.nii.gz')
+        img = torch.cat([img, spine_mask, vert_mask], dim=0)  # [3, D, H, W]
+        return img, row
+
+    def __getitem__(self, idx: int) -> Tuple[Tensor, Tensor, float]:
+        img, row = self.import_img(idx)
+        target = torch.tensor(row[self.target_cols].values.astype(np.float32))
+        return img, target, torch.nan
+
+    # TODO
+    # def import_img(self, idx: int) -> Tuple[Tensor, pd.DataFrame]:
+    #     """Returns a ``Tensor`` img with dimensions `CxDxHxW` and the corresponding ``DataFrame``
+    #     row containing the data"""
+    #     row = self.df.iloc[idx]
+    #     t2w_path = row[self.paths_key]
+    #
+    #     img = nib.load(t2w_path, mmap=False).get_fdata(dtype=np.float32)
+    #     img = self._interpolate(img)
+    #     img = self._normalize(img)
+    #     if self.augment:
+    #         img = self._augment(img)
+    #
+    #     spine_mask = self.import_mask(t2w_path, '-sag_mod-T2w_seg-spine_msk.nii.gz')
+    #     vert_mask = self.import_mask(t2w_path, '-sag_mod-T2w_seg-vert_msk.nii.gz')
+    #     img = torch.cat([img, spine_mask, vert_mask], dim=0)
+    #
+    #     return img, row
+
+    # def __getitem__(self, idx: int) -> Tuple[Tensor, Tensor, float]:
+    #     row = self.df.iloc[idx]
+    #     t2w_path = row[self.paths_key]
+    #     mask_path = t2w_path.replace('nako_data', 'nako_msk')
+    #
+    #     img = self.import_nii(t2w_path)
+    #     spine_mask = self.import_mask(mask_path, '-sag_mod-T2w_seg-spine_msk.nii.gz')
+    #     vert_mask = self.import_mask(mask_path, '-sag_mod-T2w_seg-vert_msk.nii.gz')
+    #     img = torch.cat([img, spine_mask, vert_mask], dim=0)
+    #     target = torch.tensor(row[self.target_cols].values.astype(np.float32))
+    #
+    #     return img, target, torch.nan
+
+
+class NakoBase(BaseDataset):
+    def __init__(self, df, target_cols: str = 'coord', resolution: int = 128, augment: bool = False):
+        super().__init__(df, target_cols, resolution, augment)
+
+    @staticmethod
+    def _get_vibe_mask_path(path: str) -> str:
+        path = path.replace('/rawdata_stitched', '/derivatives-Abdominal-Segmentation')
+        _, tail = os.path.split(path)
+        path = path.replace(tail, 'segmentation.nii.gz')
+        return path
+
+    def preprocess_image(self, x: NII) -> Tensor:
+        """Convert a NIFTI image into a tensor.
+
+        .. note::
+            ``NII`` or ``Nifti1Images`` cannot be converted to tensor directly and must be first cast to np.array.
+        """
+        x = torch.tensor(x.get_array(), dtype=torch.float).unsqueeze(0)
+        x = self._interpolate(x)
+        x = self._normalize(x)
+        if self.augment:
+            x = self._augment(x)
+        return x
+
+    def preprocess_segmentation(self, x: NII) -> Tensor:
+        x = torch.tensor(x.get_array(), dtype=torch.float).unsqueeze(0)
+        x = self._interpolate(x)
+        return x
+
+
+class NakoImagesDataset(NakoBase):
+    """
+    Dataset containing T2w, in-phase and out-phase images. The T2w image is first upsampled to VIBES resolution and then
+    preprocessed as usual.
+    """
+    def __init__(self, df, target_cols: str = 'coord'):
+        super().__init__(df, target_cols)
+
+    def __getitem__(self, idx: int) -> Tuple[Tensor, Tensor, float]:
+        t2w_img_path = self.df.iloc[idx][self.paths_key]
+        vibe_img_path = t2w_img_path.replace('/T2w', '/vibe')
+        inphase_img_path = vibe_img_path.replace('-sag_T2w.nii.gz', '-ax_part-inphase_vibe.nii.gz')
+        outphase_img_path = vibe_img_path.replace('-sag_T2w.nii.gz', '-ax_part-outphase_vibe.nii.gz')
+
+        inphase_image = NII.load(inphase_img_path, False)
+        outphase_image = NII.load(outphase_img_path, False)
+        t2w_image = NII.load(t2w_img_path, False)
+        t2w_image = t2w_image.resample_from_to(inphase_image, verbose=False)   # up-/resample to match VIBE resolution
+
+        inphase_image = self.preprocess_image(inphase_image)
+        outphase_image = self.preprocess_image(outphase_image)
+        t2w_image = self.preprocess_image(t2w_image)
+
+        img = torch.cat([t2w_image, inphase_image, outphase_image], dim=0)
+        target = torch.tensor(self.df.iloc[idx][self.target_cols].values.astype(np.float32))
+        return img, target, torch.nan
+
+
+class NakoImagesSegDataset(NakoBase):
+    """For training a network: 3 images + seg_mask -> STO. Uses images and segmentations masks from NAKO dataset. """
+    def __init__(self, df, target_cols: str = 'coord'):
+        super().__init__(df, target_cols)
+
+    def __getitem__(self, idx: int) -> Tuple[Tensor, Tensor, float]:
+        t2w_img_path = self.df.iloc[idx][self.paths_key]
+        t2w_seg_path = t2w_img_path.replace('/rawdata_stitched', '/derivatives_spine_inference_combination162_148')
+        t2w_seg_path = t2w_seg_path.replace('-sag_T2w.nii.gz', '-sag_mod-T2w_seg-vert_msk.nii.gz')
+        vibe_img_path = t2w_img_path.replace('/T2w', '/vibe')
+        inphase_img_path = vibe_img_path.replace('-sag_T2w.nii.gz', '-ax_part-inphase_vibe.nii.gz')
+        outphase_img_path = vibe_img_path.replace('-sag_T2w.nii.gz', '-ax_part-outphase_vibe.nii.gz')
+        vibe_seg_path = self._get_vibe_mask_path(vibe_img_path)
+
+        vibe_img_inphase = NII.load(inphase_img_path, False)
+        vibe_img_outphase = NII.load(outphase_img_path, False)
+        vibe_seg = NII.load(vibe_seg_path, True)
+        t2w_img = NII.load(t2w_img_path, False)
+        t2w_seg = NII.load(t2w_seg_path, True)
+        t2w_img = t2w_img.resample_from_to(vibe_img_inphase, verbose=False)    # up-/resample to VIBE resolution
+        t2w_seg = t2w_seg.resample_from_to(vibe_seg, verbose=False)            #
+
+        vibe_img_inphase = self.preprocess_image(vibe_img_inphase)
+        vibe_img_outphase = self.preprocess_image(vibe_img_outphase)
+        vibe_seg = self.preprocess_segmentation(vibe_seg)
+        t2w_img = self.preprocess_image(t2w_img)
+        t2w_seg = self.preprocess_segmentation(t2w_seg)
+
+        img = torch.cat([vibe_img_inphase, vibe_img_outphase, vibe_seg, t2w_img, t2w_seg], dim=0)
+        target = torch.tensor(self.df.iloc[idx][self.target_cols].values.astype(np.float32))
+        return img, target, torch.nan
+
+
+# CT Datasets
+class ComprDataset(BaseDataset):
+    """For training a ResNet: img -> compression forces
+
+    .. note::
+        For 'allResults.csv' use 'y' as target, so 'y0', ..., 'y4' are sampled.
+    """
+    def __init__(self, df, target_cols='compr'):
+        super().__init__(df, target_cols)
 
 
 class ShearComprDataset(BaseDataset):
