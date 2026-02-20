b74301586e895f6a4d662fb406710bb8dcf3a032
kbudkiewicz <krystianbudkiewicz@gmail.com>
2026-02-20 16:35:48 +0100

    feat: add a second axis to rmse plot

--- utils/eval.py
+++ utils/eval.py
@@ -274,11 +274,12 @@ def plot_rmse(
     plot_name: Optional[str] = None,
 ):
     def rmse(df: pd.DataFrame):
-        "Calcualte Root Mean Squared Error from Absolute Errors."
         return np.sqrt(df.pow(2).mean())
 
     fig, ax = plt.subplots(figsize=(8, 5))
+    ax2 = ax.twinx()
     factor = 0
+    offset_ticks = (width * len(eval_files)) / 2
 
     for file, label in eval_files:
         offset = width * factor
@@ -288,19 +289,21 @@ def plot_rmse(
             df = remove_outliers(df)
         if isinstance(stack, Iterable):
             df = stack_df_columns(df, stack)
-        x = np.arange(len(df.columns)) * len(eval_files) // 3
+        x = np.arange(len(df.columns)) * len(eval_files) // 3 + offset
         rmse_vals = rmse(df)
-        bar = ax.bar(x + offset, rmse_vals.round(2), width=width, label=label, log=True)
+        torques, forces = rmse_vals[:3], rmse_vals[3:]
+        ax.bar(x[:3], torques.round(2), width=width, label=label, log=False)
                    # yerr=df.std(), ecolor='k', capsize=3)  # error bar kwargs
-        ax.bar_label(bar, fmt='')
+        ax2.bar(x[3:], forces.round(2), width=width, label=label, log=False)
         factor += 1
 
     if stack == list(_COORD_TO_ACTUAL.keys()):
-        ax.set_xticks(x + width * len(eval_files) / 2, _COORD_TO_ACTUAL.values(), rotation=45)
+        ax.set_xticks(x - offset_ticks, _COORD_TO_ACTUAL.values(), rotation=45)
     else:
-        ax.set_xticks(x + width * len(eval_files) / 2, df.columns, rotation=90)
-    plt.ylabel('Log RMSE')
-    plt.legend(bbox_to_anchor=(1.02, 0.8))
+        ax.set_xticks(x - offset_ticks, df.columns, rotation=90)
+    ax.set_ylabel('RMSE [Nm]')
+    ax2.set_ylabel('RMSE [N]')
+    plt.legend(bbox_to_anchor=(1.12, 0.8))
     plt.grid(axis='y', linewidth=0.5, linestyle='--')
     plt.tight_layout()
 
