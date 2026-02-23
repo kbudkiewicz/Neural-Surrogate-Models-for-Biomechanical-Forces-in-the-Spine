c79d272930b1d3f6f22df76195f0d2e4b9170259
kbudkiewicz <krystianbudkiewicz@gmail.com>
2026-02-23 14:42:47 +0100

    feat: add names for coord_x

--- utils/eval.py
+++ utils/eval.py
@@ -12,6 +12,37 @@ from torch.nn import Module
 from torch.utils.data import DataLoader
 
 
+_COORDS = [f'coord_{i}' for i in range(6)]
+_COORD_TO_ACTUAL: dict = {'coord_0': r'$M_{x}$',
+                          'coord_1': r'$M_{y}$',
+                          'coord_2': r'$M_{z}$',
+                          'coord_3': r'$F_{x}$',
+                          'coord_4': r'$F_{y}$',
+                          'coord_5': r'$F_{z}$'}
+_MUSCLES: list = ['F_Ribcage_musc_rect_abdom_l',
+                  'F_Ribcage_musc_rect_abdom_r',
+                  'G_lateral__G_external_oblique__G_left',
+                  'G_lateral__G_external_oblique__G_right',
+                  'G_lateral__G_iliocostalis_lumborum__G_left',
+                  'G_lateral__G_iliocostalis_lumborum__G_right',
+                  'G_lateral__G_internal_oblique__G_lefte',
+                  'G_lateral__G_internal_oblique__G_right',
+                  'G_lateral__G_longissimus_thoracis__G_left',
+                  'G_lateral__G_longissimus_thoracis__G_right',
+                  'G_lateral__G_psoas_major__G_left__F_Pelvis',
+                  'G_lateral__G_psoas_major__G_right__F_Pelvis_L',
+                  'G_lateral__G_quadratus_lumborum__G_left',
+                  'G_lateral__G_quadratus_lumborum__G_left',
+                  'G_lateral__G_quadratus_lumborum__G_right',
+                  'G_lateral__G_quadratus_lumborum__G_right',
+                  'G_medial__G_interspinales__F',
+                  'G_medial__G_interspinales__F_T12',
+                  'G_medial__G_multifidus__G_lumbar__G_left',
+                  'G_medial__G_multifidus__G_lumbar__G_left',
+                  'G_medial__G_multifidus__G_lumbar__G_right']
+_PRELIMINARIES: list = ['compr', 'shear', *_MUSCLES]
+
+
 def relative_error(pred: Tensor, target: Tensor) -> Tensor:
     return torch.abs(target - pred) / (torch.abs(target) + 1e-8)
 
